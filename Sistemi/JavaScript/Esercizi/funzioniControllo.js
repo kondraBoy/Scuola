4fd79a6ec4eb4dda873e198146b3d943d0ed6b5d
kondraBoy <guarinoandreamio@gmail.com>
2024-11-19 09:21:47 +0100

    update funzioniControllo.js, tabPotenze.html

--- Sistemi/JavaScript/Esercizi/funzioniControllo.js
+++ Sistemi/JavaScript/Esercizi/funzioniControllo.js
@@ -1,17 +1,14 @@
-function controllaNum(num){
-	let check;
+function controllaNum(n){
+	let check = true;
 	if (isNaN(n) || n < 0)
 		check=false;
-	else
-		check=true;
 	return check;
 } 
         
 function controllaStr(str){
-	let check;
+	let check = true;
 	if(nome!=null || nome!=""){
 		check=false;
-	}else
-		check=true;
+	}
 	return check;
 }
