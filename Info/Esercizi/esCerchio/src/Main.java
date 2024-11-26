e6ebf938d5d55696f0fa6248a105f5f7731fa741
kondraBoy <andreagiochi007@gmail.com>
2024-11-26 21:47:18 +0100

    Modificato esCerchio

--- Info/Esercizi/esCerchio/src/Main.java
+++ Info/Esercizi/esCerchio/src/Main.java
@@ -1,9 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        /*Cerchio cerchio1 = new Cerchio();
+        Cerchio cerchio1 = new Cerchio();
         Cerchio cerchio2 = new Cerchio(10,6,6);
-        cerchio1.appartiene(1,1);*/
-        System.out.println("Benvenuti all'open day");
+        cerchio1.appartiene(1,1);
+
+
 
 
 
