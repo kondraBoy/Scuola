b08788b8bce9d454eaae0b9479662f2986dd2c32
kondraBoy <guarinoandreamio@gmail.com>
2024-12-17 09:00:08 +0100

    funzioniControllo.js & whatNumber.html

--- Sistemi/JavaScript/Esercizi/funzioniControllo.js
+++ Sistemi/JavaScript/Esercizi/funzioniControllo.js
@@ -1,6 +1,7 @@
+//Guarino Andrea 4BI funzioniControllo.js
 function controllaNum(n){
 	let check = true;
-	if (isNaN(n) || n < 0)
+	if (isNaN(n) || n==null)
 		check=false;
 	return check;
 } 
@@ -13,13 +14,13 @@ function controllaStr(str){
 	return check;
 }
 
-function leggiNum(msg){
+function leggiNum(vmin, vmax, msg){
 	let num;
 	do{
-		num=prompt(msg,1);
-		if(!controllaNum(num)){
-			alert("Il numero è minore di 0 oppure non è stato inserito un numero");
+		num = prompt(msg,1);
+		if(!controllaNum(num) && (num < vmin || num > vmax){
+			alert("Errore, inserisci un numero che sia maggiore di " + min + " e minore di " + max);
 		}
-	}while(!controllaNum(num));
+	}while(!controllaNum(num) && (num < vmin || num > vmax));
 	return num;
 }
