725b5393505c32fd75da4ef5e82c85e6da56c95b
kondraBoy <andreagiochi007@gmail.com>
2025-01-08 13:37:29 +0100

    esCalcolatrice

--- Info/Esercizi/provaUI/src/main/java/com/example/provaui/HelloApplication.java
+++ Info/Esercizi/provaUI/src/main/java/com/example/provaui/HelloApplication.java
@@ -1,3 +1,11 @@
+/*
+Andrea Guarino 4BI
+Realizzare un’applicazione grafica che permetta ad un utente di effettuare la somma di 2 numeri.
+L’interfaccia dovrà contenere 2 textField dove l’utente potrà inserire i numeri da sommare, un bottone e
+una label dove verrà visualizzata la somma.
+
+
+*/
 package com.example.provaui;
 
 import javafx.application.Application;
@@ -11,7 +19,7 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
--- Info/Esercizi/provaUI/src/main/java/com/example/provaui/HelloController.java
+++ Info/Esercizi/provaUI/src/main/java/com/example/provaui/HelloController.java
@@ -1,16 +1,48 @@
 package com.example.provaui;
 
-import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+        import javafx.event.ActionEvent;
+        import javafx.fxml.FXML;
+        import javafx.scene.control.Button;
+        import javafx.scene.control.Label;
+        import javafx.scene.control.TextField;
+        import javafx.scene.input.KeyEvent;
 
 public class HelloController {
-    @FXML
-    private Button btnMsgCiao;
 
     @FXML
-    private Label lblMsg;
+    private Button btnAdd;
 
-    public void onbtnMsgCiaoClick(){
-        lblMsg.setText("Benvenuti nell'applicazione")
+    @FXML
+    private Label lblExc;
+
+    @FXML
+    private TextField txtNmb1;
+
+    @FXML
+    private TextField txtNmb2;
+
+    @FXML
+    private TextField txtNmbResult;
+
+    @FXML
+    void onbtnAddClick(ActionEvent event) {
+        int n1=0, n2=0, somma;
+        try{
+            n1=Integer.parseInt(this.txtNmb1.getText());
+            n2=Integer.parseInt(this.txtNmb2.getText());
+        }
+        catch (NumberFormatException e){
+            lblExc.setText("ERRORE: NON SONO STATI INSERITI DEI NUMERI");
+        }
+        somma=n1+n2;
+        this.txtNmbResult.setText(String.valueOf(somma));
     }
+
+    @FXML
+    void removeErrors(KeyEvent event) {
+        this.lblExc.setText(" ");
+    }
+
 }
+
+
